cf6572080b46cfdd12c724b5260fe46ff1075190
Treefunder Incorporated <ceo@treefunder.com>
2014-10-26 21:30:38 -0400

    WIF Prefix fix

--- lib/czarcoin.py
+++ lib/czarcoin.py
@@ -387,7 +387,7 @@ def wif_prefix (is_test):
     if is_test:
         return b'\xf1'
     else:
-        return b'\x31'
+        return b'\x4e'
 
 def private_key_to_public_key (private_key_wif):
     # allowable_wif_prefixes = [
