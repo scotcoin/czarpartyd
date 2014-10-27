ce4ffd56acaea2258e273f0dd91e07d624627983
Treefunder Incorporated <ceo@treefunder.com>
2014-10-26 21:15:08 -0400

    WIF Prefix fix

--- lib/czarcoin.py
+++ lib/czarcoin.py
@@ -393,7 +393,8 @@ def private_key_to_public_key (private_key_wif):
     # allowable_wif_prefixes = [
     try:
     #    secret_exponent, compressed = wif_to_tuple_of_secret_exponent_compressed(private_key_wif, is_test=config.TESTNET)
-     secret_exponent, compressed = wif_to_tuple_of_secret_exponent_compressed(private_key_wif, [wif_prefix(is_test=config.TESTNET)])
+        print(private_key_wif)
+        secret_exponent, compressed = wif_to_tuple_of_secret_exponent_compressed(private_key_wif, [wif_prefix(is_test=config.TESTNET)])
     except EncodingError:
         raise exceptions.AltcoinSupportError('pycoin: unsupported WIF prefix')
     public_pair = public_pair_for_secret_exponent(generator_secp256k1, secret_exponent)
